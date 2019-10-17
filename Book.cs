b1bda0db561ec57edcdce46690d1aafd427700c5
Popular Belbase <popu0003@edu.easj.dk>
2019-10-17 13:01:06 +0200

    added file

---
+++ Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace MandatoryAssignment
+{
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private int _pageNo;
+        private string _isbn;
+
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new Exception("Title should be more than 2 characters");
+                }
+
+                _title = value;
+            }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public int PageNo
+        {
+            get { return _pageNo; }
+            set
+            {
+                if (value > 10 && value <= 1000)
+                {
+                    _pageNo = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Your page number is out of range");
+                }
+            }
+        }
+
+        public string ISBN
+        {
+            get { return _isbn; }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    _isbn = value;
+                }
+                else
+                {
+                    throw new ArgumentException("ISBN must be 13 Characters");
+
+                }
+
+            }
+        }
+
+        public Book(string title, string author, int pageNo, string isbn)
+        {
+            _title = title;
+            _author = author;
+            _pageNo = pageNo;
+            _isbn = isbn;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title} Author: {Author} , Page No : {PageNo} , ISBN:{ISBN}";
+        }
+    }
+}
